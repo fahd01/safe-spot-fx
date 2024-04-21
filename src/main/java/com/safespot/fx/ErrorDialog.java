a435cc8566e72305af6f2c3122da98291a24f60b
fahd01 <elgayed.fahd@esprit.tn>
2024-04-21 16:44:00 +0200

    Remove un-used imports from ErrorDialog.java

--- src/main/java/com/safespot/fx/ErrorDialog.java
+++ src/main/java/com/safespot/fx/ErrorDialog.java
@@ -1,32 +1,14 @@
 package com.safespot.fx;
 
-import com.safespot.fx.dao.LoanDaoImpl;
-import com.safespot.fx.model.Loan;
-import com.safespot.fx.utils.ComponentsUtils;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
-import javafx.stage.Modality;
-
-import java.io.IOException;
-import java.math.BigDecimal;
-import java.util.Objects;
-import java.util.Optional;
 
 public class ErrorDialog {
-
 
     public ErrorDialog(String errorMessage) {
 
         Alert alert = new Alert(Alert.AlertType.ERROR, errorMessage, ButtonType.CLOSE);
         alert.showAndWait();
 
-        if (alert.getResult() == ButtonType.YES) {
-            //do stuff
-    }
-
-
+        if (alert.getResult() == ButtonType.YES) {}
     }
 }
